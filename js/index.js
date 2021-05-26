2126b0af36ca8068f72d89af206669fe77034d86
ivannalis <71641625+ivannalis@users.noreply.github.com>
2021-05-26 12:28:30 +0700

    shareable

--- js/index.js
+++ js/index.js
@@ -1,51 +1,83 @@
-const IMG_FOLDER = "assets/images/"
-const GEOJSON_FOLDER = "assets/geojson/dale.geojson"
-const IMG_FORMAT = '.jpg'
+
 
 mapboxgl.accessToken =
-    'pk.eyJ1IjoiaXZhbm5hbGlzIiwiYSI6ImNrbzM0Y2c5ZjBzOTAydmpudXdtcnBuZTYifQ.BRwjq1JbwZfZOty3CnXTXA';
+'pk.eyJ1IjoiaXZhbm5hbGlzIiwiYSI6ImNrbzM0Y2c5ZjBzOTAydmpudXdtcnBuZTYifQ.BRwjq1JbwZfZOty3CnXTXA';
+
+const sheet_url = "https://docs.google.com/spreadsheets/d/1OLCtK5Re4UiZMHd2M6PBrY0Iy_NKtlkV"
+const latlong_sheet = 'dataset'
+
+const circle_radius = 5
+const point_color = 'green' 
+
+const transformRequest = (url, resourceType) => {
+    var isMapboxRequest =
+        url.slice(8, 22) === "api.mapbox.com" ||
+        url.slice(10, 26) === "tiles.mapbox.com";
+    return {
+        url: isMapboxRequest ?
+            url.replace("?", "?pluginName=sheetMapper&") :
+            url
+    };
+};
 
 const map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-3.931,50.3465], // starting position [lng, lat]
-    zoom: 18 // starting zoom
+    center: [-3.931, 50.3465], // starting position [lng, lat]
+    zoom: 18, // starting zoom
+    transformRequest: transformRequest
 });
 
 map.addControl(new mapboxgl.NavigationControl());
 
-map.on('load', function () {
-    map.addSource("location", {
-        "type": "geojson",
-        "data": GEOJSON_FOLDER
-    });
-
-    map.addLayer({
-        "id": "location",
-        "type": "circle",
-        "source": "location",
-        "paint": {
-            "circle-radius": 5,
-            "circle-color": "#ff0000"
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: sheet_url + "/gviz/tq?tqx=out:csv&sheet=" + latlong_sheet,
+        dataType: "text",
+        success: function (csvData) {
+            makeGeoJSON(csvData);
         }
     });
-});
 
-map.on('click', function (e) {
-    var features = map.queryRenderedFeatures(e.point, {
-        layers: ['location'] // replace this with the name of the layer
-    });
+    function makeGeoJSON(csvData) {
+        csv2geojson.csv2geojson(csvData, {
+            latfield: 'latitude',
+            lonfield: 'longitude',
+            delimiter: ','
+        }, function (err, data) {
+            map.on('load', function () {
+                map.addLayer({
+                    'id': 'csvData',
+                    'type': 'circle',
+                    'source': {
+                        'type': 'geojson',
+                        'data': data
+                    },
+                    'paint': {
+                        'circle-radius': circle_radius,
+                        'circle-color': point_color
+                    }
+                });
+            });
 
-    if (!features.length) {
-        return;
+            map.on('click', function (e) {
+                var features = map.queryRenderedFeatures(e.point, {
+                    layers: ['csvData'] // replace this with the name of the layer
+                });
+
+                if (!features.length) {
+                    return;
+                }
+                let feature = features[0];
+                let popup = new mapboxgl.Popup({
+                        offset: [0, 0]
+                    })
+                    .setLngLat(feature.geometry.coordinates)
+                    .setHTML(`<p style="text-align:center;" class="center">Image Name </p><h3 style="font-size:18px; font-weight:bold;text-align:center;">${feature.properties.preview_name}</h3><iframe width="100%" height="250px" allowfullscreen style="border-style:none;" src="/imgprev.html?thumb=${feature.properties.thumb}&show_high=${feature.properties.image_link}"></iframe> `) // CHANGE THIS TO REFLECT THE PROPERTIES YOU WANT TO SHOW
+                    .setLngLat(feature.geometry.coordinates)
+                    .addTo(map);
+            });
+        });
     }
-    let feature = features[0];
-    let popup = new mapboxgl.Popup({
-            offset: [0, 0]
-        })
-        .setLngLat(feature.geometry.coordinates)
-        .setHTML(`<p style="text-align:center;" class="center">Image Name </p><h3 style="font-size:18px; font-weight:bold;text-align:center;">${feature.properties.image_id}</h3><iframe width="350" height="250" allowfullscreen style="border-style:none;" src="https://ivannalis.github.io/dale_map_dev/imgprev.html?thumb=${feature.properties.thumb}&show_high=${feature.properties.image_link}"></iframe> `) // CHANGE THIS TO REFLECT THE PROPERTIES YOU WANT TO SHOW
-        .setLngLat(feature.geometry.coordinates)
-        .addTo(map);
-
-});
+})
